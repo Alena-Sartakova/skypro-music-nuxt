39277331748a2ed481b7e992416c93d2af9a89ed
Alena <sartakovaalenka@gmail.com>
2025-07-22 22:19:40 +0700

    layout

---
+++ components/MainSideBarComponent.vue
@@ -0,0 +1,143 @@
+<template>
+  <div class="main__sidebar sidebar">
+    <div class="sidebar__personal">
+      <p class="sidebar__personal-name">Sergey.Ivanov</p>
+      <div class="sidebar__icon">
+        <svg>
+          <use xlink:href="#logout" />
+        </svg>
+      </div>
+    </div>
+    <div class="sidebar__block">
+      <div class="sidebar__list">
+        <div
+          v-for="playlist in playlists"
+          :key="playlist.id"
+          class="sidebar__item"
+          :class="{ active: $route.params.id === playlist.id }"
+        >
+          <NuxtLink class="sidebar__link" :to="`/category/${playlist.id}`">
+            <img
+              class="sidebar__img"
+              :src="getImagePath(playlist.image)"
+              :alt="playlist.title"
+            />
+          </NuxtLink>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const playlists = [
+  {
+    id: "day",
+    title: "Плейлист дня",
+    image: "playlist01.png",
+  },
+  {
+    id: "dance",
+    title: "100 танцевальных хитов",
+    image: "playlist02.png",
+  },
+  {
+    id: "indie",
+    title: "Инди-заряд",
+    image: "playlist03.png",
+  },
+];
+
+// Функция для формирования пути к изображению
+const getImagePath = (imageName) => {
+  return new URL(`../assets/img/${imageName}`, import.meta.url).href;
+};
+</script>
+
+<style lang="css">
+.main__sidebar {
+  width: 418px;
+  flex: 0 0 auto;
+  padding: 20px 90px 20px 78px;
+}
+.sidebar__personal {
+  display: -webkit-box;
+  display: -ms-flexbox;
+  display: flex;
+  -webkit-box-orient: horizontal;
+  -webkit-box-direction: normal;
+  -ms-flex-direction: row;
+  flex-direction: row;
+  -webkit-box-align: center;
+  -ms-flex-align: center;
+  align-items: center;
+  -webkit-box-pack: end;
+  -ms-flex-pack: end;
+  justify-content: flex-end;
+  padding: 12px 0 15px 0;
+}
+
+.sidebar__personal-name {
+  font-style: normal;
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 24px;
+  color: #ffffff;
+  margin-right: 16px;
+}
+
+.sidebar__icon {
+  width: 43px;
+  height: 43px;
+  background-color: #313131;
+  border-radius: 50%;
+  cursor: pointer;
+}
+
+.sidebar__block {
+  height: 100%;
+  padding: 240px 0 0 0;
+  display: -webkit-box;
+  display: -ms-flexbox;
+  display: flex;
+  -webkit-box-orient: vertical;
+  -webkit-box-direction: normal;
+  -ms-flex-direction: column;
+  flex-direction: column;
+  -webkit-box-pack: start;
+  -ms-flex-pack: start;
+  justify-content: flex-start;
+}
+
+.sidebar__list {
+  display: -webkit-box;
+  display: -ms-flexbox;
+  display: flex;
+  -webkit-box-orient: vertical;
+  -webkit-box-direction: normal;
+  -ms-flex-direction: column;
+  flex-direction: column;
+  -webkit-box-align: center;
+  -ms-flex-align: center;
+  align-items: center;
+}
+
+.sidebar__item {
+  width: 250px;
+  height: 150px;
+}
+
+.sidebar__item:not(:last-child) {
+  margin-bottom: 30px;
+}
+
+.sidebar__link {
+  width: 100%;
+  height: 100%;
+}
+
+.sidebar__img {
+  width: 100%;
+  height: auto;
+}
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,49 @@
+<template>
+<div class="wrapper">
+    <div class="container">
+      <div class="main">
+        <NavbarComponent/>
+          <div class="main__centerblock">
+            <SearchComponent />
+            <slot></slot>
+          </div>
+        <MainSideBarComponent />
+        </div>
+      <PlayerBarComponent />
+    </div>
+    <footer class="footer"></footer>
+  </div>
+</template>
+
+<script setup>
+import MainSideBarComponent from '~/components/MainSideBarComponent.vue';
+import NavbarComponent from '~/components/NavbarComponent.vue';
+import PlayerBarComponent from '~/components/PlayerBarComponent.vue';
+
+
+</script>
+
+<style lang="scss" scoped>
+.wrapper {
+  width: 100%;
+  min-height: 100%;
+  background-color: #383838;
+  font-family: "Roboto", sans-serif;
+}
+
+.container {
+  max-width: 1920px;
+  width: 100%;
+  padding: 0;
+  margin: 0 auto;
+
+  background-color: #181818;
+}
+
+.main__centerblock {
+  flex: 1;
+  min-width: 0;
+  padding: 20px 20px calc(111px - 20px);
+  margin-left: 0;
+}
+</style>
